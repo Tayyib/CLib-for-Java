5f8b70f223c0dc4c9e9a7531eb2154b05453b59f
Tayyib <m.tayyib.yel@gmail.com>
2017-07-31 21:11:11 +0300

    Add JavaFX.transitions

---
+++ JavaFX/src/clib/javafx/transitions/FadeEffect.java
@@ -0,0 +1,39 @@
+package clib.javafx.transitions;
+
+
+import javafx.animation.Transition;
+import javafx.scene.Node;
+import javafx.util.Duration;
+
+
+public class FadeEffect extends Transition
+{
+    private final Node node;
+    private final double fromValue;
+    private final double diff;
+
+
+    public FadeEffect(Node node, int duration, double toValue)
+    {
+        setCycleDuration(Duration.millis(duration));
+
+        this.node = node;
+        this.fromValue = node.getOpacity();
+        this.diff = toValue - fromValue;
+    }
+
+    public FadeEffect(Node node, int duration, double fromValue, double toValue)
+    {
+        setCycleDuration(Duration.millis(duration));
+
+        this.node = node;
+        this.fromValue = fromValue;
+        this.diff = toValue - fromValue;
+    }
+
+    @Override
+    protected final void interpolate(double frac)
+    {
+        node.setOpacity(fromValue + (diff * frac));
+    }
+}
---
+++ JavaFX/src/clib/javafx/transitions/ScaleEffect.java
@@ -0,0 +1,42 @@
+package clib.javafx.transitions;
+
+
+import com.sun.istack.internal.Nullable;
+import javafx.animation.Transition;
+import javafx.scene.Node;
+import javafx.util.Duration;
+
+
+public class ScaleEffect extends Transition
+{
+    private final Node node;
+    private final double fromX;
+    private final double fromY;
+    private final double fromZ;
+    private final double diffX;
+    private final double diffY;
+    private final double diffZ;
+
+
+    public ScaleEffect(Node node, int duration, @Nullable Double toX, @Nullable Double toY, @Nullable Double toZ)
+    {
+        setCycleDuration(Duration.millis(duration));
+
+        this.node = node;
+        this.fromX = node.getScaleX();
+        this.fromY = node.getScaleY();
+        this.fromZ = node.getScaleZ();
+
+        this.diffX = toX == null ? 0 : toX - fromX;
+        this.diffY = toY == null ? 0 : toY - fromY;
+        this.diffZ = toZ == null ? 0 : toZ - fromZ;
+    }
+
+    @Override
+    protected void interpolate(double frac)
+    {
+        node.setScaleX(fromX + (diffX * frac));
+        node.setScaleY(fromY + (diffY * frac));
+        node.setScaleZ(fromZ + (diffZ * frac));
+    }
+}
