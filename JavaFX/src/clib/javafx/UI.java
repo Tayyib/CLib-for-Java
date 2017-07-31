fd35fe2cd6e5e0633be06078cf4fed6c03009565
Tayyib <m.tayyib.yel@gmail.com>
2017-08-01 01:47:44 +0300

    Update javafx.UI.java

--- JavaFX/src/clib/javafx/UI.java
+++ JavaFX/src/clib/javafx/UI.java
@@ -13,7 +13,6 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 
-
 @SuppressWarnings("unused")
 public class UI
 {
@@ -46,7 +45,7 @@ public class UI
     public static void switchScene(Stage stage, Scene currentScene, Scene nextScene)
     {
         FadeTransition effect1 = new FadeTransition(currentScene.getRoot(), 300, 0);
-        FadeTransition effect2 = new FadeTransition(nextScene.getRoot(), 510, 0,1);
+        FadeTransition effect2 = new FadeTransition(nextScene.getRoot(), 510, 0, 1);
 
         nextScene.getRoot().setOpacity(0);
 
