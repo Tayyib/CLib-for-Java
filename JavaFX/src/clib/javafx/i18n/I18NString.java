73338544020b97a3de827b02223ae37529a5af52
Tayyib <m.tayyib.yel@gmail.com>
2018-02-03 00:56:38 +0300

    Update JavaFX.I18N
    - Java 9 fix.

--- JavaFX/src/clib/javafx/i18n/I18NString.java
+++ JavaFX/src/clib/javafx/i18n/I18NString.java
@@ -1,6 +1,8 @@
 package clib.javafx.i18n;
 
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
@@ -13,6 +15,23 @@ public class I18NString extends SimpleStringProperty
     private String key;
     private Object[] arguments;
     private ResourceBundle bundle;
+
+    private static Method translateMethod;
+    static
+    {
+        int jvnumber = Character.getNumericValue(System.getProperty("java.version").charAt(0));
+
+        try
+        {
+            translateMethod = jvnumber < 9
+                    ? I18NString.class.getDeclaredMethod("translate", ResourceBundle.class, String.class)
+                    : I18NString.class.getDeclaredMethod("translate9", ResourceBundle.class, String.class);
+        }
+        catch (NoSuchMethodException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
 
     private static ResourceBundle getBundle(String bundleKey)
@@ -27,7 +46,22 @@ public class I18NString extends SimpleStringProperty
             byte[] byteArray = bundle.getString(key).getBytes(StandardCharsets.ISO_8859_1);
             return new String(byteArray, StandardCharsets.UTF_8);
         }
-        catch (MissingResourceException e) { return String.format("null::%s", key); }
+        catch (MissingResourceException e)
+        {
+            return String.format("null::%s", key);
+        }
+    }
+
+    private static String translate9(ResourceBundle bundle, String key)
+    {
+        try
+        {
+            return bundle.getString(key);
+        }
+        catch (MissingResourceException e)
+        {
+            return String.format("null::%s", key);
+        }
     }
 
     I18NString(String bundleKey)
@@ -48,14 +82,30 @@ public class I18NString extends SimpleStringProperty
     {
         key = newValue;
         arguments = null;
-        super.set(translate(bundle, newValue));
+
+        try
+        {
+            super.set((String) translateMethod.invoke(I18NString.class, bundle, newValue));
+        }
+        catch (IllegalAccessException | InvocationTargetException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     void set(String newValue, Object... args)
     {
         key = newValue;
         arguments = args;
-        super.set(String.format(translate(bundle, newValue), args));
+
+        try
+        {
+            super.set(String.format((String) translateMethod.invoke(I18NString.class, bundle, newValue), args));
+        }
+        catch (IllegalAccessException | InvocationTargetException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     void update(Object... args)
